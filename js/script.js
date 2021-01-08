8cf8519ddc34728c918ee593aef28eb96f07b05b
Abdelhamid-bedair <abdelhamid.bedair@gmail.com>
2021-01-09 00:26:05 +0200

    Delete circles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
+/*============ To Get HTML inputs' values ==========*/
 var taskName = document.getElementById("taskName");
 var taskDate = document.getElementById("taskDate");
 var taskNotes = document.getElementById("taskNotes");
 
-if (localStorage.getItem("storedTasks") == null) {
-    tasksContainer = [];
-}
-else {
-    var tasksContainer = JSON.parse(localStorage.getItem("storedTasks"));
-    displayTasks();
-}
-
+/*============ To Add new task then save all tasks array it in the local storage ==========*/
 function addNewTask() {
     var task = {
         name: taskName.value,
@@ -19,7 +13,8 @@ function addNewTask() {
     tasksContainer.push(task);
     localStorage.setItem("storedTasks", JSON.stringify(tasksContainer))
     displayTasks();
-    clearForm();
+    clearForm();//reset form
+    downFunction();//scrolling down to the last task 
 };
 
 function clearForm() {
@@ -35,24 +30,57 @@ function displayTasks() {
             `<div class="card text-center my-3">
             <div class="card-header">`+ tasksContainer[i].date + `</div>
             <div class="card-body">
-            <h4 class="card-title">`+ (i + 1 + "/") + tasksContainer[i].name + `</h4>
+            <span class="badge badge-light">`+ (i + 1) +`</span>
+            <h4 class="card-title">`+ tasksContainer[i].name + `</h4>
              <p>`+ tasksContainer[i].notes + `</p></div>
             <div class="card-footer text-muted">
-            <button class="btn btn-light"><i class="far fa-edit editIcon"></i>Edit</button>
-            <button onclick="deleteTask(`+ i + `)" class="btn btn-light"><i class="far fa-calendar-minus deleteIcon"></i>Delete</button></div></div>`
+            <button onclick="editTaskInputs(`+ i + `)" class="btn btn-light"><i class="far fa-edit editIcon"></i></button>
+            <button onclick="deleteTask(`+ i + `)" class="btn btn-light"><i class="far fa-calendar-minus deleteIcon"></i></button></div></div>`
     }
     document.getElementById("todoList").innerHTML = displayedTasks;
 };
 
 function deleteTask(taskIndex) {
-
     tasksContainer.splice(taskIndex, 1);
     localStorage.setItem("storedTasks", JSON.stringify(tasksContainer))
     displayTasks();
-}
+};
 
 function deleteAll() {
     tasksContainer = [];
     localStorage.setItem("storedTasks", JSON.stringify(tasksContainer))
+    displayTasks();
+};
+
+function editTaskInputs(taskIndex) {
+    taskName.value = tasksContainer[taskIndex].name;
+    taskDate.value = tasksContainer[taskIndex].date;
+    taskNotes.value = tasksContainer[taskIndex].notes;
+    document.getElementById("addOrEditBtn").innerHTML = `<i class="far fa-save editIcon"></i>save`;
+    topFunction();
+    editTask();
+};
+
+function editTask() {
+    if (document.getElementById("addOrEditBtn").innerHTML = `<i class="far fa-save editIcon"></i>save`); {
+        addOrEditBtn.onclick = console.log("a");
+
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+function downFunction() {
+    window.scrollTo(0, document.body.scrollHeight);
+};
+
+/*============ Saving date to local storage ==========*/
+if (localStorage.getItem("storedTasks") == null) {
+    tasksContainer = [];
+}
+else {
+    var tasksContainer = JSON.parse(localStorage.getItem("storedTasks"));
     displayTasks();
 }
